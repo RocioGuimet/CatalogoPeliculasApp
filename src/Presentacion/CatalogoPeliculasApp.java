221657edc80104caad44a8b40f04452209585dab
unknown <rocioguimet@hotmail.com>
2025-11-07 17:11:30 -0300

    Creación de clase presentación

---
+++ src/Presentacion/CatalogoPeliculasApp.java
@@ -0,0 +1,61 @@
+package Presentacion;
+
+import dominio.Pelicula;
+import service.IServicePeliculas;
+import service.ServicePeliculasLista;
+
+import java.util.Scanner;
+
+public class CatalogoPeliculasApp {
+    static void main(String[] args) {
+        var salir = false;
+        var consola = new Scanner(System.in);
+        // Agregamos la implementacion del service
+        IServicePeliculas servicePeliculas = new ServicePeliculasLista();
+        while(!salir){
+            try{
+                mostrarMenu();
+                salir = ejecutarOpciones(consola, servicePeliculas);
+            } catch (Exception e){
+                System.out.println("Ocurrió un eror: " + e.getMessage());
+            }
+            System.out.println();
+        } //fin while
+    }
+
+    //Implementamos metodos
+    private static void mostrarMenu(){
+        System.out.println("""
+                * Catálogo de películas *
+                1. Agregar película
+                2. Listar películas
+                3. Buscar película
+                4. Salir
+                """);
+    }
+
+    private static boolean ejecutarOpciones(Scanner consola, IServicePeliculas servicePeliculas){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        switch (opcion) {
+            case 1 -> {
+                System.out.println("Introduce el nombre de la película: ");
+                var nombrePelicula = consola.nextLine();
+                servicePeliculas.agregarPelicula(new Pelicula(nombrePelicula));
+            }
+            case 2 -> servicePeliculas.listarPeliculas();
+
+            case 3 -> {
+                System.out.println("Introduce la película a buscar: ");
+                var buscar = consola.nextLine();
+                servicePeliculas.buscarPelicula(new Pelicula (buscar));
+            }
+            case 4 -> {
+                System.out.println("¡Hasta pronto!");
+                salir = true;
+            }
+            default -> System.out.println("Opción incorrecta: " + opcion);
+        } // fin switch
+        return salir;
+    }
+}
